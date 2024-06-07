30e99a7351ec8e55e18b9624b6ce51ebd012e296
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-07 14:22:41 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
         <div className="Header">
           <div className="Header_inner">
             <ul className="H_left">
-              <li><a href="#none">영남일보</a></li>
-              <li><span className="bold">Today</span><sapn className="date">2024년 5월 25일 금요일</sapn></li>
+              <li className="ynlink"><a href="#none">영남일보</a></li>
+              <li className="todate"><span className="bold">Today</span><sapn className="date">2024년 5월 25일 금요일</sapn></li>
             </ul>
             <a href="#none" className="logo">영남일보AI</a>
           </div>
