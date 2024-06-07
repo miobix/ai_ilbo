65e39a823c924bb938c54ee4d2b1cdb2db88bfbc
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-07 16:54:22 +0900

    Update page.js

--- src/app/sub_news/page.js
+++ src/app/sub_news/page.js
@@ -4,7 +4,26 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-
+          {/*Header*/}
+        <div className="Header">
+          <div className="Header_inner">
+            <ul className="H_left">
+              <li className="ynlink"><a href="#none">영남일보</a></li>
+              <li className="todate"><span className="bold">Today</span><sapn className="date">2024년 5월 25일 금요일</sapn></li>
+            </ul>
+            <a href="#none" className="logo">영남일보AI</a>
+          </div>
+        </div>
+    
+        <div className="sub_inner">
+        </div>
+  
+        {/*Footer*/}
+        <div className="Footer">
+          <div className="Fotter_inner">
+            <p className="copyrights">영남일보 AI © 2024 YeongNamAI. All rights reserved.</p>
+          </div>
+        </div>  
     </main>
   );
 }
