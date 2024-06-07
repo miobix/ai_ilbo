9216900c0dd0dd49e74a3a3dbfb48edcdeef2d99
miobix <c.aravena90@gmail.com>
2024-06-07 10:34:23 +0900

    empty project
    new ynai work

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,95 @@
+import Image from "next/image";
 import styles from "./page.module.css";
-import Header from "@/components/Header/Header";
-import TopNews from "@/components/NewsGroups/TopNewsSection/TopNews";
-import CategorySection from "@/components/NewsGroups/CategorySection/CategorySection";
-import AllNewsSection from "@/components/NewsGroups/AllNewsSection/AllNewsSection";
-import Footer from "@/components/Footer/Footer";
-
 
 export default function Home() {
-
   return (
     <main className={styles.main}>
-      <section>
-        <Header />
-        <TopNews />
-        <CategorySection />
-        <div className={styles.allNewsContainer}>
-          <AllNewsSection />
-        </div>  
-        <Footer />
-      </section>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+          <p>Explore starter templates for Next.js.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
   );
 }
--- src/components/UI/BannerButton/BannerButton.js
+++
@@ -1,9 +0,0 @@
-import styles from "./BannerButton.module.css";
-
-export default function BannerButton({ props, children }) {
-  return (
-    <div className={styles.container}>
-      <p className={styles.text}>{children}</p>
-    </div>
-  );
-}
