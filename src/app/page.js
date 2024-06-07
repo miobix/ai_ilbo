dd38bce69d689d4476a8f4cd7ab3639702edd3f5
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-07 14:46:03 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -26,6 +26,7 @@ export default function Home() {
                 <p className="tit">경북도, 국힘에 TK특별법 제정과 저출산 극복 등 국가투자 요청</p>
                 <p className="sub_tit">경북도가 22대 국회 개원 후 처음으로 5일 서울 켄싱턴호텔에서 열린 국민의 힘과의 예산정책협의회에서 대구경북통합특별법과 저출산 극복등에 대한 국가투자 지원을 요청했다.</p>
                 <p className="img"></p>
+                <p className="date">2024-06-05 12:20</p>
               </a>           
             </div>
             {/*News List*/}
