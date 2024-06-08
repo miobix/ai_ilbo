09dccf971787d4dccb53b76c077a2d43ff1559ec
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-08 20:44:20 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
     <main className={styles.main}>      
     
-        {/*Header*/}
-        <div className={styles.Header}>
-          <div className={styles.Header_inner}>
-            <ul className={styles.H_left}>
-              <li className={styles.ynlink}><a href="#none">영남일보</a></li>
-              <li className={styles.todate}><span className={styles.bold}>Today</span><sapn className={styles.date}>2024년 5월 25일 금요일</sapn></li>
-            </ul>
-            <a href="#none" className={styles.logo}>영남일보AI</a>
-          </div>
-        </div>
-    
+        <Header />
         <div className={styles.main_inner}>
           {/*Main News*/}
           <div className={styles.Main_cont}>
