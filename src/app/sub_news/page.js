8d2c6ef73930bb8e295ef1dbda0815ff51b6d847
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-08 21:00:39 +0900

    Update page.js

--- src/app/sub_news/page.js
+++ src/app/sub_news/page.js
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
 
 export default function Home() {
   return (
     <main className={styles.main}>
-          {/*Header*/}
-        <div className="Header">
-          <div className="Header_inner">
-            <ul className="H_left">
-              <li className="ynlink"><a href="#none">영남일보</a></li>
-              <li className="todate"><span className="bold">Today</span><sapn className="date">2024년 5월 25일 금요일</sapn></li>
-            </ul>
-            <a href="#none" className="logo">영남일보AI</a>
-          </div>
-        </div>
+         <Header />
     
         {/*SUB article*/}
         <div className="sub_inner">
