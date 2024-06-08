49298d3735ac3d903cec6c7f5995cf566d696c01
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-08 20:54:18 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-
+import TopNews from "@/components/NewsGroups/TopNewsSection/TopNews";
 
 export default function Home() {
   return (
@@ -13,16 +13,7 @@ export default function Home() {
           {/*Main News*/}
           <div className={styles.Main_cont}>
             <div className={styles.Main_cont_inner}>
-              <h2 className={styles.M_title}>AI NEWS</h2>
-              {/*Top News*/}
-              <div className={styles.M_top_news}>
-                <a href="#none">
-                  <p className={styles.tit}>경북도, 국힘에 TK특별법 제정과 저출산 극복 등 국가투자 요청</p>
-                  <p className={styles.sub_tit}>경북도가 22대 국회 개원 후 처음으로 5일 서울 켄싱턴호텔에서 열린 국민의 힘과의 예산정책협의회에서 대구경북통합특별법과 저출산 극복등에 대한 국가투자 지원을 요청했다.</p>
-                  <p className={styles.img}><img src="/sample_01.png" /></p>
-                  <p className={styles.date}>2024-06-05 12:20</p>
-                </a>           
-              </div>
+              <TopNews />
               {/*News List*/}
               <div className={styles.M_list_news}>
                 <ul className={styles.li_news}>
