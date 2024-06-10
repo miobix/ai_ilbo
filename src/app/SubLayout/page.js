681710ba7c9266b3ed8f66394c5d47d06cdd3dfe
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-10 14:44:25 +0900

    Update page.js

--- src/app/SubLayout/page.js
+++ src/app/SubLayout/page.js
@@ -19,7 +19,7 @@ export default function Home() {
                 <SubRelated />
               </div>
                <SubPaging />
-               <a href="#none" className="ListBtn">목록</a>
+               <a href="#none" className={styles.ListBtn}>목록</a>
             </div>
         </div>
     
