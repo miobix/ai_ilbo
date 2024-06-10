55cbfa1871cf17faaf7ac528d15f4844fa222d12
baenana86 <97072081+baenana86@users.noreply.github.com>
2024-06-10 10:10:09 +0900

    Update page.js

--- src/app/SubLayout/page.js
+++ src/app/SubLayout/page.js
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Footer from "../components/NewsGroups/SubArticles/SubArticles";
+import Footer from "../components/NewsGroups/SubRelated/SubRelated";
 
 export default function Home() {
   return (
@@ -13,57 +14,7 @@ export default function Home() {
         <div className={styles.sub_inner}>
           <div className={styles.article_section}>
             <SubArticles />
-            {/*related article section*/}
-            <div className={styles.related}>
-              <h4 className={styles.tit}>관련뉴스</h4>
-              <ul className={styles.related_list}>
-                <li>
-                  <a href="#none">
-                    <p className={styles.img}><img src="/sample_12.png" /></p>
-                    <div className={styles.cnt}>
-                      <p className={styles.tit}>일본정부, 한국 국립해양조사원 독도주변 해양조사에 항의</p>
-                      <p className={styles.date}>2024.06.07</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="#none">
-                    <p className={styles.img}><img src="/sample_13.png" /></p>
-                    <div className={styles.cnt}>
-                      <p className={styles.tit}>[속보] 아브레우 고문20% 추정 성공률 양호하고 높은 수준</p>
-                      <p className={styles.date}>2024.06.07</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="#none">
-                    <p className={styles.img}><img src="/sample_14.png" /></p>
-                    <div className={styles.cnt}>
-                      <p className={styles.tit}>경주시, 교육발전특구 지정에 힘쓴다.</p>
-                      <p className={styles.date}>2024.06.07</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="#none">
-                    <p className={styles.img}><img src="/sample_12.png" /></p>
-                    <div className={styles.cnt}>
-                      <p className={styles.tit}>일본정부, 한국 국립해양조사원 독도주변 해양조사에 항의</p>
-                      <p className={styles.date}>2024.06.07</p>
-                    </div>
-                  </a>
-                </li>
-                <li>
-                  <a href="#none">
-                    <p className={styles.img}><img src="/sample_12.png" /></p>
-                    <div className={styles.cnt}>
-                      <p className={styles.tit}>일본정부, 한국 국립해양조사원 독도주변 해양조사에 항의</p>
-                      <p className={styles.date}>2024.06.07</p>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <SubRelated />
           </div>
            {/*paging news section*/}
            <div className={styles.paging_news}>
