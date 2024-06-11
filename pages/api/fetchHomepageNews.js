3e5e4ce721cea90f02f50143ba74523f00cd7533
miobix <c.aravena90@gmail.com>
2024-06-11 11:31:22 +0900

    Update fetchHomepageNews.js

--- pages/api/fetchHomepageNews.js
+++ pages/api/fetchHomepageNews.js
@@ -17,18 +17,24 @@ export default async function fetchHomepageNews(req, res) {
       const data = await collection.find({}).toArray();
       const reversedData = data.reverse();
 
-       // Extract summary IDs from the fetched data
        const summaryIds = reversedData.map(item => item.summary_id);
-       // Fetch summaries from the 'summaries' collection based on the summary IDs
+
       const summariesCollection = database.collection('summaries');
       const summaries = await summariesCollection.find({ _id: { $in: summaryIds } }).toArray();
-      // Combine the fetched data with the summaries
+
       const mergedData = reversedData.map(item => {
         const summary = summaries.find(summary => summary._id.equals(item.summary_id));
         return { ...item, summary };
       });
 
-      res.status(200).json(mergedData);
+      // Validate items to send back
+      const validatedData = mergedData.filter(item => 
+        item.summary_id && 
+        item.generated_img_url && 
+        item.generated_img_url.original
+      );
+
+      res.status(200).json(validatedData);
     } catch (error) {
       res.status(500).json({ message: 'Something went wrong' });
     } finally {
