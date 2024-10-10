0e5238a41ab940fda3d8298bc00b9cf569e469e7
miobix <c.aravena90@gmail.com>
2024-10-10 17:45:40 +0900

    Update route.js

--- src/app/feed.xml/route.js
+++ src/app/feed.xml/route.js
@@ -8,31 +8,27 @@ export async function GET() {
         generator: 'RSS for Node and Next.js',
         feed_url: 'https://yeongnam.ai/feed.xml', 
         site_url: 'https://yeongnam.ai',
-        // feed_url: 'http://localhost:3000/feed.xml',
-        // site_url: 'http://localhost:3000',
+        //feed_url: 'http://localhost:3000/feed.xml',
+        //site_url: 'http://localhost:3000',
        
         managingEditor: 'naturei.dev@gmail.com (NatureI)',
         webMaster: 'naturei.dev@gmail.com (NatureI)',
         language: 'ko-KR',
         pubDate: new Date().toUTCString(),
-        ttl: 2,
+        ttl: 30,
     });
     // ${req.nextUrl.origin}
     try {
       const response = await fetch(`https://yeongnam.ai/api/fetchAllPress`);
-      //const response = await fetch(`http://localhost:3000/api/fetchAllPress`);
+      // const response = await fetch(`http://localhost:3000/api/fetchAllPress`);
       if (!response.ok) {
           console.error("Failed to fetch from fetchAllPress:", response.statusText);
           return new Response('Error fetching news', { status: 500 });
       }
 
       const allNews = await response.json();
-      console.log(allNews.length);
-
-      
-
+     
       allNews.forEach(item => {
-          console.log(item.timestamp)
           feed.item({
               title: item.summary.title || 'Default Title',
               description: item.summary.article_body || 'No description provided', 
